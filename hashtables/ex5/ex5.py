54b13fd16e5901a4f461f21eecb937806d5650b9
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-08-07 09:53:51 -0700

    ex5 done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,27 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    paths = {}
 
+    for path in files:
+        index = len(path) - 1
+        char = path[index]
+        key = ''
+
+        while char != '/':
+            key = char + key
+            index -= 1
+            char = path[index]
+
+        index = index + 1
+        if key in paths:
+            paths[key].append(path[:index])
+        else:
+            paths[key] = [path[:index]]
+
+    for q in queries:
+        if q in paths:
+            for i in range(len(paths[q])):
+                result.append(f'{paths[q][i]}{q}')
     return result
 
 
